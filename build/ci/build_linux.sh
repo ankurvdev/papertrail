5f76667af00aecf52b4232d5b336887659cda426
ankurv <ankurv@microsoft.com>
2023-07-28 08:52:23 -0700

    Remove build

--- build/ci/build_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-set -o pipefail
-set -x
-
-cmake -DCMAKE_CXX_FLAGS=-stdlib=libstdc++ $(dirname $(realpath $0))/..
-cmake --build . -j --target package
-ctest --output-on-failure
--- build/cmake/use_cxxopts.cmake
+++
@@ -1,5 +0,0 @@
-function(link_to_target targetName)
-    vcpkg_download(cxxopts)
-    find_package(cxxopts CONFIG REQUIRED)
-    target_link_libraries(${targetName} PRIVATE cxxopts::cxxopts)
-endfunction()
