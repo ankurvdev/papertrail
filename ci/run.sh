dfe5ff714b64540d4aaeb3efebe5e908e704d7f2
Ankur Verma <ankurv@microsoft.com>
2023-10-29 00:33:25 -0700

    Fixes

---
+++ ci/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+set -o pipefail
+set -x
+
+scriptdir=$(dirname "${BASH_SOURCE[0]}")
+image_name="papertrail_build"
+
+init() {
+    if [ ! -x "$(command -v podman)" ]; then sudo apt-get -y install podman; fi
+}
+
+build() {
+    init
+    podman build -t papertrail_build --rm -f $scriptdir/../Dockerfile
+    podman image save -o $1 papertrail_build
+}
+
+test() {
+    init
+    if [[ ! $(podman image list | grep ${image_name}) ]]; then
+        if [ -e "$1" ]; then echo "Cannot find source container image (\$1): $1;" && exit -1; fi
+        cat "$1" | podman image import - ${image_name}
+    fi
+    podman run --rm localhost/${image_name}
+}
+
+cmd=$1
+shift
+$cmd $*
--- r.py
+++ r.py
@@ -10,25 +10,25 @@ import buildverse.svelte
 
 src_dir = Path(__file__).parent
 parser = argparse.ArgumentParser()
-parser.add_argument('--work-dir', type=Path, default=src_dir / 'work')
+parser.add_argument("--work-dir", type=Path, default=src_dir / "work")
 args = parser.parse_args()
 
 work_dir: Path = args.work_dir
 work_dir.mkdir(exist_ok=True, parents=True)
 
-venv_dir = work_dir / 'venv'
+venv_dir = work_dir / "venv"
 if not venv_dir.exists():
     subprocess.check_call([sys.executable, "-m", "venv", venv_dir.as_posix()])
     subprocess.check_call([(venv_dir / "bin" / "pip").as_posix(), "install", "-r", (src_dir / "requirements.txt").as_posix()])
 
-typesense_bin = buildverse.externaltools.GetBinary('typesense-server', binpath=work_dir)
+typesense_bin = buildverse.externaltools.GetBinary("typesense-server", binpath=work_dir)
 
-svelte_dir = work_dir / 'svelte'
-(work_dir / 'bin').mkdir(exist_ok=True)
-os.environ['DEVEL_BUILDPATH'] = svelte_dir.as_posix()
-os.environ['DEVEL_BINPATH'] = (work_dir / 'bin').as_posix()
-os.environ['NPM_BUILD_ROOT'] = (work_dir /'npm').as_posix()
-builder = buildverse.svelte.SvelteBuilder(reporoot=src_dir, subdir=src_dir / 'web', buildroot=svelte_dir, out_file_list=None)
+svelte_dir = work_dir / "svelte"
+(work_dir / "bin").mkdir(exist_ok=True)
+os.environ["DEVEL_BUILDPATH"] = svelte_dir.as_posix()
+os.environ["DEVEL_BINPATH"] = (work_dir / "bin").as_posix()
+os.environ["NPM_BUILD_ROOT"] = (work_dir / "npm").as_posix()
+builder = buildverse.svelte.SvelteBuilder(reporoot=src_dir, subdir=src_dir / "web", buildroot=svelte_dir, out_file_list=None)
 builder.generate()
 builder.build()
 print(src_dir)
